40adff1efc9c35693745a0fac63406e714a525d4
emerge <i.i.babko@gmail.com>
2016-04-09 19:27:11 +0300

    fix gulpfile and add additional npm dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -24,10 +24,10 @@ elixir(function(mix) {
     });
 
     mix
-        .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
-        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap');
-        .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
-        .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
-        .copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css');
+        .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js')
+        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap')
+        .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css')
+        .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
+        .copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css')
         .copy('node_modules/font-awesome/fonts', 'public/fonts');
 });
