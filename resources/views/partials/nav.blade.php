4d1bdd247abc183892b0e82144e16ac9f75c726b
emerge <i.i.babko@gmail.com>
2016-04-09 20:55:04 +0300

    style navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="Nav">
-    <div class="Logo">GyM</div>
+    <div class="Logo">GYM</div>
     <ul class="Nav__list">
         <li class="Nav__link">Log In</li>
         <li class="Nav__link">Sign Up</li>
