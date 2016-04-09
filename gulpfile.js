532924b243853fd29c71bd616a4ff5fcbbcf72f4
emerge <i.i.babko@gmail.com>
2016-04-09 19:04:38 +0300

    configure gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var elixir = require('laravel-elixir');
+var postStylus = require('poststylus');
+
+require('laravel-elixir-livereload');
+require('laravel-elixir-stylus');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +16,18 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.stylus('app.styl', null, {
+        use: [
+            postStylus( [ 'autoprefixer', 'lost', 'rucksack-css' ] )
+        ]
+    });
+
+    mix
+        .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap');
+        .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+        .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+        .copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css');
+        .copy('node_modules/font-awesome/fonts', 'public/fonts');
 });
