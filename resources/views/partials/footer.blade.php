0235eab402f62dd450ff233b739b22dcc79b1f12
emerge <i.i.babko@gmail.com>
2016-04-10 00:27:12 +0300

    add Social.styl file and improve footer styles

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,17 +2,17 @@
     <div>
         <h2 class="Footer__heading">Need help?</h2>
         <ul class="Footer__list">
-            <li class="Footer__item">FAQ</li>
-            <li class="Footer__item">Ask a question</li>
-            <li class="Footer__item">Support</li>
+            <li class="Footer__item"><a href="#" class="Footer__link">FAQ</a></li>
+            <li class="Footer__item"><a href="#" class="Footer__link">Ask a question</a></li>
+            <li class="Footer__item"><a href="#" class="Footer__link">Support</a></li>
         </ul>
     </div>
     <div>
         <h2 class="Footer__heading">Customer care</h2>
         <ul class="Footer__list">
-            <li class="Footer__item">Terms</li>
-            <li class="Footer__item">About</li>
-            <li class="Footer__item">Contacts</li>
+            <li class="Footer__item"><a href="#" class="Footer__link">Terms</a></li>
+            <li class="Footer__item"><a href="#" class="Footer__link">About</a></li>
+            <li class="Footer__item"><a href="#" class="Footer__link">Contacts</a></li>
         </ul>
     </div>
     <div>
@@ -21,12 +21,12 @@
     </div>
     <div>
         <h2 class="Footer__heading">Join us</h2>
-        <ul class="Footer__list">
-            <li class="Footer__item">f</li>
-            <li class="Footer__item">vk</li>
-            <li class="Footer__item">you</li>
-            <li class="Footer__item">ins</li>
-            <li class="Footer__item">t</li>
+        <ul class="Social">
+            <li class="Social__item"><a href="#" class="Social__link">1</a></li>
+            <li class="Social__item"><a href="#" class="Social__link">2</a></li>
+            <li class="Social__item"><a href="#" class="Social__link">3</a></li>
+            <li class="Social__item"><a href="#" class="Social__link">4</a></li>
+            <li class="Social__item"><a href="#" class="Social__link">5</a></li>
         </ul>
         <div class="Footer__copyright">&copy; 2016 sumy.gym.ua</div>
     </div>
