929135d8107c9a43bd9ceab54bff40aef187669c
emerge <i.i.babko@gmail.com>
2016-04-09 20:29:24 +0300

    delete stylus partials folder and add Nav.styl component

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,1 +1,7 @@
-<nav>|-GyM-|</nav>
+<nav class="Nav">
+    <div class="Logo">GyM</div>
+    <ul class="Nav__list">
+        <li class="Nav__link">Log In</li>
+        <li class="Nav__link">Sign Up</li>
+    </ul>
+</nav>
