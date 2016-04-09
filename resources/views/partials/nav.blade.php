4eb33d14f4b497727110e1bf42709c211db5205b
emerge <i.i.babko@gmail.com>
2016-04-09 21:19:33 +0300

    improve navbar styles

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,9 +1,9 @@
 <nav class="Nav">
     <div class="Logo">GYM</div>
     <ul class="Nav__list">
-        <li class="Nav__link">Log In</li>
-        <li class="Nav__link">Sign Up</li>
-        <li class="Nav__link">
+        <li class="Nav__item">Log In</li>
+        <li class="Nav__item">Sign Up</li>
+        <li class="Nav__item">
             <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
         </li>
     </ul>
