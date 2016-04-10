7c22c545a53294ea73a1fce5ddb911c5067acfd9
emerge <i.i.babko@gmail.com>
2016-04-10 14:56:48 +0300

    attach header styles to section styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header class="Header">
-    <div class="Header__content">
-        <h1 class="Header__heading">Lorem ipsum dolor sit amet</h1>
-        <div class="Header__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti reprehenderit, facilis expedita minima fugiat fugit enim debitis ea vero ut modi dolorem in repudiandae illum maxime labore autem eius dicta!</div>
-        <div class="Header__media">
+<header class="Section Section--header">
+    <div class="Section__content">
+        <h1 class="Section__heading">Lorem ipsum dolor sit amet</h1>
+        <div class="Section__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti reprehenderit, facilis expedita minima fugiat fugit enim debitis ea vero ut modi dolorem in repudiandae illum maxime labore autem eius dicta!</div>
+        <div class="Section__media">
             <div class="Media">
                 <h2 class="Media__heading">Lorem ipsum</h2>
                 <img src="/img/food1.png" class="Media__picture" alt="food">
@@ -14,6 +14,6 @@
                 <div class="Media__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, eum.</div>
             </div>
         </div>
-        <button class="Header__button">Let's start</button>
+        <button class="Section__button">Let's start</button>
     </div>
 </header>
