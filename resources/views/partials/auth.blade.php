f42288d92d7589bff5d56712039c614ea2b41130
emerge <i.i.babko@gmail.com>
2016-04-22 22:39:57 +0300

    style auth form

--- resources/views/partials/auth.blade.php
+++ resources/views/partials/auth.blade.php
@@ -1,11 +1,15 @@
 <div class="Auth">
-    <form method="POST" action="{{ url('/login') }}" class="Auth__form">
-        {!! csrf_field() !!}
-        <input type="email" name="email" class="Input">
-        <input type="password" name="password" class="Input">
-        <input type="checkbox" name="remember" class="Checkbox"> Remember Me
-        <button type="submit"><i class="fa fa-btn fa-sign-in"></i>Login</button>
-        <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-    </form>
+    <div class="Auth__modal">
+        <h1 class="Auth__header">Authentication</h1>
+        <div class="Auth__body">
+            <form method="POST" action="{{ url('/login') }}" class="Auth__form">
+                {!! csrf_field() !!}
+                <input type="email" name="email" class="Input" placeholder="Email">
+                <input type="password" name="password" class="Input" placeholder="Password">
+                <button type="submit" class="Button Button--login Button--yellow">Log In</button>
+                <a href="{{ url('/password/reset') }}" class="Auth__reset-password">Forgot Your Password?</a>
+            </form>
+        </div>
+    </div>
 </div>
 <div class="Auth__overlay"></div>
