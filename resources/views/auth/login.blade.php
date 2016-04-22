6024e7d4d462219e17468bab42f5975066bea175
emerge <i.i.babko@gmail.com>
2016-04-22 16:47:12 +0300

    add login.blade.php partial

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,24 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<form method="POST" action="{{ url('/login') }}">
+    {!! csrf_field() !!}
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label >E-Mail Address</label>
+        <input type="email" name="email" value="{{ old('email') }}">
+        @if ($errors->has('email'))
+            <strong>{{ $errors->first('email') }}</strong>
+        @endif
     </div>
-</div>
-@endsection
+
+    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label>Password</label>
+        <input type="password" name="password">
+        @if ($errors->has('password'))
+            <strong>{{ $errors->first('password') }}</strong>
+        @endif
+    </div>
+
+    <input type="checkbox" name="remember"> Remember Me
+    <button type="submit">
+        <i class="fa fa-btn fa-sign-in"></i>login
+    </button>
+    <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+</form>
---
+++ resources/views/partials/auth.blade.php
@@ -0,0 +1,10 @@
+<div class="Auth">
+    <form method="POST" action="{{ url('/login') }}" class="Auth__form">
+        {!! csrf_field() !!}
+        <input type="email" name="email" class="Input">
+        <input type="password" name="password" class="Input">
+        <input type="checkbox" name="remember" class="Checkbox"> Remember Me
+        <button type="submit"><i class="fa fa-btn fa-sign-in"></i>Login</button>
+        <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+    </form>
+</div>
