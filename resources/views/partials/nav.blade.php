5c2871ffa92049016346da9a0d1819304367ed7a
emerge <i.i.babko@gmail.com>
2016-04-23 20:52:32 +0300

    add profile link to navbar and profile view

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,8 +5,12 @@
             <input type="text" class="Search__input" placeholder="search">
             <span class="glyphicon glyphicon-search Search__button" aria-hidden="true"></span>
         </li>
-        <li class="Nav__item" data-type="login">Log In</li>
-        <li class="Nav__item" data-type="register">Sign Up</li>
+        @if (Auth::check())
+            <li class="Nav__item"><a href="/profile" class="Nav__link"><span>My profile</span></a></li>
+        @else
+            <li class="Nav__item" data-type="login">Log In</li>
+            <li class="Nav__item" data-type="register">Sign Up</li>
+        @endif
     </ul>
     <li class="Nav__item Nav__item--hamburger">
         <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
