55b8ad74408e70edd909770d91062d8be5a5d300
emerge <i.i.babko@gmail.com>
2016-04-24 00:31:30 +0300

    remove gender section from index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,19 +19,4 @@
         <button class="Button Button--yellow">Let's start</button>
     </div>
 </header>
-<section class="Section Section--dark">
-    <div class="Section__content">
-        <h1 class="Section__heading">Choose Your gender</h1>
-        <div class="Section__media">
-            <div class="Media Media--gender">
-                <img src="/img/man-dark.png" class="Media__picture Media__picture--man" alt="food">
-                <!-- <button class="Button Button--blue">Male</button> -->
-            </div>
-            <div class="Media Media--gender">
-                <img src="/img/woman1-dark.png" class="Media__picture Media__picture--woman" alt="program">
-                <!-- <button class="Button Button--pink">Female</button> -->
-            </div>
-        </div>
-    </div>
-</section>
 @stop
