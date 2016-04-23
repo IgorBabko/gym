322a4d6ff8151c0dde8c38ff7d3443af7dd85bc5
emerge <i.i.babko@gmail.com>
2016-04-24 02:07:35 +0300

    add Block stylus component

--- resources/views/bmi.blade.php
+++ resources/views/bmi.blade.php
@@ -1,5 +1,31 @@
 @extends ('layout')
 
 @section ('content')
-    bmi
+<div class="Block">
+    <form method="POST" class="Form" action="{{ url('/calcBmi') }}">
+        <label class="Label Label--inline">
+            <input type="radio" name="gender" class="Radio">
+            Male
+        </label>
+        <label class="Label Label--inline">
+            <input type="radio" name="gender" class="Radio">
+            Female
+        </label>
+        <label class="Label">
+            Heigh:
+            <input type="text" name="height" class="Input">
+            <span class="Error"></span>
+        </label>
+        <label class="Label">
+            Weight:
+            <input type="text" name="weight" class="Input">
+            <span class="Error"></span>
+        </label>
+        {!! csrf_field() !!}
+        <button type="submit" class="Button Button--red">Calculate</button>
+    </form>
+    <div class="Info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quae, optio eveniet minima quidem dolorum eum! Repellendus tenetur ut nulla aut et, ipsa ad, nihil facere. Tenetur architecto atque quasi explicabo ex modi ullam, commodi culpa, debitis distinctio vitae, minima unde? Eos praesentium dolorem deserunt, nesciunt, officiis consequatur, reprehenderit consectetur mollitia atque inventore delectus. Autem, cupiditate ratione sapiente? Blanditiis iste laborum, ipsam quaerat rerum nisi velit nulla recusandae! A sed vitae dolorem earum officia aliquid odit rem ullam recusandae accusantium consequuntur cum modi ipsam odio laboriosam similique inventore culpa iure, aut. Ipsam temporibus sint ipsum modi laudantium eveniet asperiores, dolore.
+    </div>
+</div>
 @stop
