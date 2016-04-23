1feeba9ae1d6deb2c6bd623b7fe39567c234909c
emerge <i.i.babko@gmail.com>
2016-04-23 23:42:09 +0300

    add view, route, controller method for metrics page

---
+++ resources/views/metrics.blade.php
@@ -0,0 +1,21 @@
+@extends ('layout')
+
+@section ('content')
+    <div class="Section__media">
+        <div class="Media">
+            <h2 class="Media__heading">Lorem ipsum1</h2>
+            <img src="/img/food1.png" class="Media__picture" alt="food">
+            <div class="Media__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, eum.</div>
+        </div>
+        <div class="Media">
+            <h2 class="Media__heading">Lorem ipsum2</h2>
+            <img src="/img/dumbbells.jpg" class="Media__picture Media__picture--program" alt="program">
+            <div class="Media__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, eum.</div>
+        </div>
+        <div class="Media">
+            <h2 class="Media__heading">Lorem ipsum3</h2>
+            <img src="/img/dumbbells.jpg" class="Media__picture Media__picture--program" alt="program">
+            <div class="Media__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, eum.</div>
+        </div>
+    </div>
+@stop
