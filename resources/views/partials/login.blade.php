0d5a1ba8e10dd2569efc87efe12e3eda17eedac8
emerge <i.i.babko@gmail.com>
2016-04-23 15:40:45 +0300

    focus on first field when opening modal

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -2,10 +2,12 @@
     <div class="Modal__content">
         <h1 class="Modal__header">Authentication</h1>
         <div class="Modal__body">
-            <form method="POST" action="{{ url('/login') }}" class="Modal__form">
+            <form method="POST" action="{{ url('/login') }}" class="Modal__form Modal__form--login">
+                <label class="Label">Email:</label>
+                <input type="email" name="email" class="Input">
+                <label class="Label">Password:</label>
+                <input type="password" name="password" class="Input">
                 {!! csrf_field() !!}
-                <input type="email" name="email" class="Input" placeholder="Email">
-                <input type="password" name="password" class="Input" placeholder="Password">
                 <button type="submit" class="Button Button--modal Button--yellow">Log In</button>
                 <a href="{{ url('/password/reset') }}" class="Modal__reset-password">Forgot Your Password?</a>
             </form>
