689119de1925eaa453a14170c3c73a7d3893b59e
emerge <i.i.babko@gmail.com>
2016-04-24 22:47:47 +0300

    fix metrics formulas

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -2,7 +2,7 @@
 
 namespace Gym\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 use Gym\Http\Requests;
 
 class MetricsController extends Controller
@@ -32,8 +32,15 @@ class MetricsController extends Controller
 
     public function calcBmr()
     {
-        return Request::input('coeff') + 13.4 * Request::input('weight')
-                + 4.8 * Request::input('height') - 5.7 * Request::input('age');
+        $activity_level = Request::input('activity_level') ?? 1;
+
+        if (Request::input('gender') == 'male') {
+            return $activity_level * (88.36 + 13.4 * Request::input('weight')
+                + 4.8 * Request::input('height') - 5.7 * Request::input('age') );
+        } 
+
+        return $activity_level * (447.6 + 9.2 * Request::input('weight')
+                + 3.1 * Request::input('height') - 4.3 * Request::input('age') );
     }
 
     public function water()
@@ -44,9 +51,9 @@ class MetricsController extends Controller
     public function calcWater()
     {
         if (Request::input('gender') == 'male') {
-            return Request::input('weight') * 0.04 * Request::input('physical-activity') * 0.6;
+            return Request::input('weight') * 0.04 + Request::input('physical-activity') * 0.6;
         }
 
-        return Request::input('weight') * 0.03 * Request::input('physical-activity') * 0.4;
+        return Request::input('weight') * 0.03 + Request::input('physical-activity') * 0.4;
     }
 }
