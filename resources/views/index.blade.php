9b5e51788603fa66a2ddccbc21da68203218b052
emerge <i.i.babko@gmail.com>
2016-04-24 21:13:07 +0300

    add info blocks to slider items

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,35 @@
 @extends('layout')
 @section('content')
-<ul class="bxslider">
-    <li><img src="/img/1.jpg"/></li>
-    <li><img src="/img/3.jpg"/></li>
-    <li><img src="/img/4.jpg"/></li>
+<ul class="Slider bxslider">
+    <li class="Slider__item">
+        <img src="/img/1.jpg"/>
+        <div class="Slider__info Slider__info--bottom">
+            <h1 class="Slider__heading">Niko Bellic</h1>
+            <p class="Slider__description">
+                lorem ipsum dolor sit amet, consectetur adipisicing elit. fuga magni corporis explicabo esse asperiores, sit at expedita praesentium. error, rerum.
+            </p>
+            <button class="Button Button--red pull-left">more info</button>
+        </div>
+    </li>
+    <li class="Slider__item">
+        <img src="/img/3.jpg"/>
+        <div class="Slider__info Slider__info--right">
+            <h1 class="Slider__heading">Niko Bellic</h1>
+            <p class="Slider__description">
+                lorem ipsum dolor sit amet, consectetur adipisicing elit. fuga magni corporis explicabo esse asperiores, sit at expedita praesentium. error, rerum.
+            </p>
+            <button class="Button Button--red pull-left">More info</button>
+        </div>
+    </li>
+    <li class="Slider__item">
+        <img src="/img/4.jpg"/>
+        <div class="Slider__info Slider__info--top">
+            <h1 class="Slider__heading">Niko Bellic</h1>
+            <p class="Slider__description">
+                lorem ipsum dolor sit amet, consectetur adipisicing elit. fuga magni corporis explicabo esse asperiores, sit at expedita praesentium. error, rerum.
+            </p>
+            <button class="Button Button--red pull-left">More info</button>
+        </div>
+    </li>
 </ul>
 @stop
