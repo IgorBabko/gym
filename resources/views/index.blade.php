cb95e0cb47185a8ed31f3cb16a24a04f6e9c77ed
emerge <i.i.babko@gmail.com>
2016-04-26 00:20:01 +0300

    add food section to main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,9 +35,18 @@
 <div class="sectio">
     <div class="sectio__picture"></div>
     <div class="sectio__content">
+        <h1 class="sectio__heading">Some header</h1>
         lorem ipsum dolor sit amet, consectetur adipisicing elit. asperiores laudantium ut distinctio quod nesciunt labore reiciendis at placeat culpa ipsum voluptatum debitis voluptates molestiae et, mollitia inventore nostrum fuga, ea incidunt voluptas aperiam tenetur hic alias dolore quas. incidunt, laborum iusto tenetur unde libero excepturi placeat, molestias quo doloribus sapiente.
         lorem ipsum dolor sit amet, consectetur adipisicing elit. asperiores laudantium ut distinctio quod nesciunt labore reiciendis at placeat culpa ipsum voluptatum debitis voluptates molestiae et, mollitia inventore nostrum fuga, ea incidunt voluptas aperiam tenetur hic alias dolore quas. incidunt, laborum iusto tenetur unde libero excepturi placeat, molestias quo doloribus sapiente.
     </div>
+</div>
+<div class="sectio sectio--red">
+    <div class="sectio__content" style="flex-basis: 30%">
+        <h1 class="sectio__heading">Some header</h1>
+        lorem ipsum dolor sit amet, consectetur adipisicing elit. asperiores laudantium ut distinctio quod nesciunt labore reiciendis at placeat culpa ipsum voluptatum debitis voluptates molestiae et, mollitia inventore nostrum fuga, ea incidunt voluptas aperiam tenetur hic alias dolore quas. incidunt, laborum iusto tenetur unde libero excepturi placeat, molestias quo doloribus sapiente.
+        lorem ipsum dolor sit amet, consectetur adipisicing elit. asperiores laudantium ut distinctio quod nesciunt labore reiciendis at placeat culpa ipsum voluptatum debitis voluptates molestiae et, mollitia inventore nostrum fuga, ea incidunt voluptas aperiam tenetur hic alias dolore quas. incidunt, laborum iusto tenetur unde libero excepturi placeat, molestias quo doloribus sapiente.
+    </div>
+    <div class="sectio__picture sectio__picture--food"></div>
 </div>
 <div class="sectio sectio--parallax"></div>
 @stop
