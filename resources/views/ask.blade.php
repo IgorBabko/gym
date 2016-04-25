f093cc8de8e1147d961ff7d288f1cd1599f6712b
emerge <i.i.babko@gmail.com>
2016-04-25 19:53:31 +0300

    add scroll magic plugin

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -2,8 +2,8 @@
 
 @section ('content')
 <div class="Block">
-    <h1 class="Block__heading">Body Mass Index</h1>
-    <form method="post" class="form" action="{{ url('/metrics/calcBmi') }}">
+    <h1 class="Block__heading" style="text-align: center">Questions list</h1>
+    <form method="post" class="Form Form--question" action="{{ url('/metrics/calcBmi') }}">
         <label class="Label">
             Name:
             <input type="text" name="name" class="Input">
@@ -15,11 +15,59 @@
             <span class="Error"></span>
         </label>
         <label class="Label">Question</label>
-        <textarea class="Textarea">niko bellic</textarea>
+        <textarea class="Textarea"></textarea>
         {!! csrf_field() !!}
         <button type="submit" class="Button Button--red">Ask</button>
     </form>
     <div class="Block__description">
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="Question">
+            <h2 class="Question__title">niko bellic</h2>
+            <div class="Question_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
+        <div class="question">
+            <h2 class="question__title">niko bellic</h2>
+            <div class="question_text">lorem ipsum dolor sit amet, consectetur adipisicing elit. quam expedita reprehenderit natus dicta, quaerat cum dolorum voluptas facere consequatur atque.</div>
+        </div>
     </div>
 </div>
 @stop
