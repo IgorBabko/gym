ce8cadf4c371cc50e967264d28892424ab17ab55
emerge <i.i.babko@gmail.com>
2016-05-02 13:59:14 +0300

    add nicolaslopezj/searchable package and adjust Page model to be searchable

--- app/Page.php
+++ app/Page.php
@@ -3,8 +3,23 @@
 namespace Gym;
 
 use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Page extends Model
 {
-    protected $fillable = ['name', 'text']; 
+    use SearchableTrait;
+
+    protected $fillable = ['name', 'text'];
+
+    /**
+     * Searchable rules.
+     *
+     * @var array
+     */
+    protected $searchable = [
+        'columns' => [
+            'name' => 10,
+            'text' => 10,
+        ],
+    ];
 }
